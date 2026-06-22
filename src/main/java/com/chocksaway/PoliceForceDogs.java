d5af80dd4407823302213fed807931801476537d
Miles Davenport <milesd@chocksaway.com>
2026-06-22 12:23:30 +0100

    Main app.

---
+++ src/main/java/com/chocksaway/PoliceForceDogs.java
@@ -0,0 +1,11 @@
+package com.chocksaway;
+
+import io.micronaut.runtime.Micronaut;
+
+// Main
+public class PoliceForceDogs {
+
+    public static void main(String[] args) {
+        Micronaut.run(PoliceForceDogs.class, args);
+    }
+}
